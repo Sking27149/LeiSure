6e644ec1f3f7086ed2289bbe012a96a49fbf166d
Sking27149 <antilxx@163.com>
2016-06-13 10:13:21 +0800

    Initial Commit

---
+++ LeiSure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeiSure
+//
+//  Created by lanou on 16/6/13.
+//  Copyright © 2016年 Sking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
